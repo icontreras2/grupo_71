24a3e2956b1c8b6c61cdb9be886acb92f2f060dd
Martin Sanchez <martinsanchez@MacBook-Air-de-Martin.local>
2021-05-28 18:24:34 -0400

    consulta2_php

--- Sites/consultas/consulta_2.php
+++ Sites/consultas/consulta_2.php
@@ -5,26 +5,25 @@
   #Llama a conexión, crea el objeto PDO y obtiene la variable $db
   require("../config/conexion.php");
 
-  $id_nuevo = $_POST["id_elegido"];
+  $comuna = $_POST["comuna_elegida"];
 
- 	$query = "SELECT * FROM pokemones where pid = $id_nuevo;";
+ 	$query = "SELECT Trabajadores.id, Trabajadores.nombre, Trabajadores.rut, Trabajadores.edad, Trabajadores.sexo FROM Trabajadores, Tiendas, Direcciones, TiendasTrabajadores, Comunas WHERE TiendasTrabajadores.id_tienda = Tiendas.id AND Tiendas.id_jefe = TiendasTrabajadores.id_trabajador AND Tiendas.id_direccion = Direcciones.id AND Direcciones.id_comuna = Comunas.id AND Comunas.nombre = '$comuna';";
 	$result = $db -> prepare($query);
 	$result -> execute();
-	$pokemones = $result -> fetchAll();
+	$jefes = $result -> fetchAll();
   ?>
 
 	<table>
     <tr>
       <th>ID</th>
       <th>Nombre</th>
-      <th>Altura</th>
-      <th>Peso</th>
-      <th>Experiencia Base</th>
-      <th>Tipo</th>
+      <th>RUT</th>
+      <th>edad</th>
+      <th>sexo</th>
     </tr>
   <?php
-	foreach ($pokemones as $pokemon) {
-  		echo "<tr><td>$pokemon[0]</td><td>$pokemon[1]</td><td>$pokemon[2]</td><td>$pokemon[3]</td><td>$pokemon[4]</td><td>$pokemon[5]</td></tr>";
+	foreach ($jefes as $jefe) {
+  		echo "<tr><td>$jefe[0]</td><td>$jefe[1]</td><td>$jefe[2]</td><td>$jefe[3]</td><td>$jefe[4]</td></tr>";
 	}
   ?>
 	</table>
