a27a6b5ee5fdbfd2849da4e48e15241d33f2cec6
Martin Sanchez <martinsanchez@MacBook-Air-de-Martin.local>
2021-07-06 21:16:04 -0400

    procedimiento importar personal

---
+++ Entrega3/procedimientos/importar_personal.sql
@@ -0,0 +1,42 @@
+CREATE OR REPLACE FUNCTION
+
+-- declaramos la función y sus argumentos
+importar_personal (nombre varchar(100), rut varchar(20), edad int, sexo varchar(10), contraseña varchar(20), id_direccion int)
+
+-- declaramos lo que retorna 
+RETURNS BOOLEAN AS $$
+
+-- declaramos las variables a utilizar si es que es necesario
+DECLARE
+id_max_usuario int;
+
+-- definimos nuestra función
+BEGIN
+
+    -- control de flujo
+
+    IF 'contraseña' NOT IN (SELECT column_name FROM information_schema.columns WHERE table_name='usuarios') THEN
+        ALTER TABLE usuarios ADD contraseña;
+
+
+    IF rut IN (SELECT rut FROM usuarios;) THEN
+        RETURNS FALSE;
+    
+    ELSE
+        SELECT INTO id_max_usuario
+        MAX(id)
+        FROM usuarios;
+
+        insert into usuarios values(id_max_usuario + 1, nombre, rut, edad, sexo, contraseña);
+        insert into direccionesusuario values(id_max_usuario + 1, id_direccion);
+
+        RETURNS TRUE;
+
+    END IF;
+
+
+
+
+-- -- finalizamos la definición de la función y declaramos el lenguaje
+END
+$$ language plpgsql
