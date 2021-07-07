47fb707352d51ab7e561d87239bcd413fa4a76ff
Martin Sanchez <martinsanchez@MacBook-Air-de-Martin.local>
2021-07-07 11:42:03 -0400

    a

--- Entrega3/procedimientos/importar_personal.sql
+++ Entrega3/procedimientos/importar_personal.sql
@@ -16,7 +16,7 @@ BEGIN
     -- control de flujo
 
     IF 'contraseña' NOT IN (SELECT column_name FROM information_schema.columns WHERE table_name='usuarios') THEN
-        ALTER TABLE usuarios ADD contraseña;
+        ALTER TABLE usuarios ADD contraseña varchar(20);
 
 
     IF rut IN (SELECT rut FROM usuarios;) THEN
