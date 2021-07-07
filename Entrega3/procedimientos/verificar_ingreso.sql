3eaddebb0c0dad56ce04d7943b00590aaf5aa28e
Martin Sanchez <martinsanchez@MacBook-Air-de-Martin.local>
2021-07-07 11:30:45 -0400

    procedimiento verificar ingreso

---
+++ Entrega3/procedimientos/verificar_ingreso.sql
@@ -0,0 +1,31 @@
+CREATE OR REPLACE FUNCTION
+
+-- declaramos la función y sus argumentos
+verificar_ingreso (rut varchar(20), contraseña varchar(20))
+
+-- declaramos lo que retorna 
+RETURNS BOOLEAN AS $$
+
+-- declaramos las variables a utilizar si es que es necesario
+DECLARE
+usuario;
+
+-- definimos nuestra función
+BEGIN
+
+    -- control de flujo
+    IF rut NOT IN (SELECT rut FROM usuarios;) THEN
+        RETURNS FALSE;
+    
+    ELSE IF contraseña NOT IN (SELECT contraseña FROM usuarios WHERE usuarios.rut = rut) THEN
+        RETURNS FALSE;
+    
+    ELSE
+        RETURNS TRUE;
+        
+    END IF;
+
+
+-- -- finalizamos la definición de la función y declaramos el lenguaje
+END
+$$ language plpgsql
